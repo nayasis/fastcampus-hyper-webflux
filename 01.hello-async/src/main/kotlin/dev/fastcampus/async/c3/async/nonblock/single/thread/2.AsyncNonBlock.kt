f08fbccc3381a7b04710bacee0b97abde2d391f3
nayasis <nayasis@gmail.com>
2023-07-18 21:19:53 +0900

    동기/비동기 예제 수정

--- 01.hello-async/src/main/kotlin/dev/fastcampus/async/c3/async/nonblock/single/thread/2.AsyncNonBlock.kt
+++ 01.hello-async/src/main/kotlin/dev/fastcampus/async/c3/async/nonblock/single/thread/2.AsyncNonBlock.kt
@@ -1,15 +1,17 @@
 package dev.fastcampus.async.c3.async.nonblock.single.thread
 
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
+@OptIn(DelicateCoroutinesApi::class)
 suspend fun main() {
-    val singleThread = Dispatchers.Default.limitedParallelism(1)
+    val singleThread = newSingleThreadContext("single")
     coroutineScope {
         launch(singleThread) { workHard() }
         launch(singleThread) { drinkCoffee() }
