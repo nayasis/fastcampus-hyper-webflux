002ad17277e9016d53dee350b9dca1f3d090ff75
nayasis <nayasis@gmail.com>
2023-08-10 21:54:02 +0900

    data class 처럼 변경

--- 03.spring-webflux/3-2.webflux-reactor/src/main/kotlin/dev/fastcampus/webflux/model/Article.kt
+++ 03.spring-webflux/3-2.webflux-reactor/src/main/kotlin/dev/fastcampus/webflux/model/Article.kt
@@ -7,14 +7,25 @@ import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
 @Table("TB_ARTICLE")
-class Article {
+class Article(
     @Id
-    var id: Long = 0
-    var title: String? = null
-    var body: String? = null
-    var authorId: Long? = null
+    var id: Long = 0,
+    var title: String? = null,
+    var body: String? = null,
+    var authorId: Long? = null,
     @CreatedDate
-    var createdAt: LocalDateTime? = null
+    var createdAt: LocalDateTime? = null,
     @LastModifiedDate
-    var updatedAt: LocalDateTime? = null
+    var updatedAt: LocalDateTime? = null,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Article
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
